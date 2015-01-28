edd42f3500118ea305148f3ebd2c27c797671c8d
Michael Henretty <michael.henretty@gmail.com>
2015-01-27 17:34:13 -0800

    desploy app changes

--- dist/app/js/globals.js
+++ dist/app/js/globals.js
@@ -4,7 +4,8 @@ define(["exports"], function (exports) {
   window.COMPONENTS_BASE_URL = "./components/";
   require.config({
     paths: {
-      "gaia-dialog": "components/gaia-dialog/gaia-dialog"
+      "gaia-dialog": "components/gaia-dialog/gaia-dialog",
+      "gaia-component": "components/gaia-component/gaia-component"
     }
   });
 });
