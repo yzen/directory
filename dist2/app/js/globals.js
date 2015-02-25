4937477e44830ed33cc34971dc810cc0a6bccaf4
Michael Henretty <michael.henretty@gmail.com>
2015-02-25 06:46:16 -0800

    Pushing new version to test differences

---
+++ dist2/app/js/globals.js
@@ -0,0 +1,11 @@
+define(["exports"], function (exports) {
+  "use strict";
+
+  window.COMPONENTS_BASE_URL = "./components/";
+  require.config({
+    paths: {
+      "gaia-dialog": "components/gaia-dialog/gaia-dialog",
+      "gaia-component": "components/gaia-component/gaia-component"
+    }
+  });
+});
